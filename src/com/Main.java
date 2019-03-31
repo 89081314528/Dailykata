2013f1a9a40471a0842d251ad0a14afc3c668f74
89081314528 <89081314528@mail.ru>
2019-03-31 23:16:21 +0300

    изменение 31.03

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,7 +1,60 @@
 package com;
 
+/** Массив с днями недели
+ * Метод, который распечатывает все элементы массива
+ * Метод, который рспечатывает выходные
+ * Метод, который считает сумму, метод, который считает произведение
+ * Метод applyOperation, который в зависимости от номера операции возвращает сумму или произведение
+ * Метод, который считает степень числа
+ * Добавить метод, который считает степень в applyOperation
+ */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        String[] weekDay = new String[4];
+        weekDay[0] = "Monday";
+        weekDay[1] = "Thusday";
+        weekDay[2] = "Sutturday";
+        weekDay[3] = "Sunday";
+        printWeekDay(weekDay);
+        printWeekends(weekDay);
+        System.out.println(sum(4,2));
+        System.out.println(product(4,2));
+        System.out.println(applyOperation(4,2, 1));
+        System.out.println(applyOperation(4,2, 2));
+        System.out.println(applyOperation(4,2, 3));
+        System.out.println(stepen(4,2));
+    }
+    public static void printWeekDay(String[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+    public static void printWeekends(String[] a) {
+        for (int i = 2; i < 4; i++) {
+            System.out.println(a[i]);
+        }
+    }
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+    public static int product(int a,int b) {
+        return a * b;
+    }
+    public static int applyOperation(int a, int b, int operation) {
+        if (operation == 1) {
+            return sum(a, b);
+        }
+        if (operation == 2) {
+            return product(a, b);
+        }
+        return stepen(a, b);
+    }
+    public static int stepen(int a, int b) {
+        int chislo = a;
+        for (int i = 1; i < b; i ++) {
+            a = a * chislo;
+        }
+        return a;
     }
 }
