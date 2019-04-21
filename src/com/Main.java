064a65335b7ed22fbd7a4dccd5397de12ff5184b
89081314528 <89081314528@mail.ru>
2019-04-21 11:02:00 +0300

    изменение 21/04

--- src/com/Main.java
+++ src/com/Main.java
@@ -2,7 +2,6 @@ package com;
 
 /** Массив с днями недели
  * Метод, который распечатывает все элементы массива
- * Метод, который рaспечатывает выходные
  * Метод, который считает сумму, метод, который считает произведение
  * Метод applyOperation, который в зависимости от номера операции возвращает сумму или произведение
  * Метод, который считает степень числа
@@ -11,28 +10,30 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
-       String[] day = new String[4];
-       day[0] = "a";
-       day[1] = "s";
-       day[2] = "d";
-       day[3] = "f";
-       printDay(day);
-        System.out.println(appleOperation(4,2,1));
-        System.out.println(appleOperation(4,2,2));
-        System.out.println(appleOperation(4,2,3));
+      String[] day = new String[3];
+      day[0] = "a";
+      day[1] = "s";
+      day[2] = "d";
+      printDays(day);
+        System.out.println(operation(4,2,1));
+        System.out.println(operation(4,2,2));
+        System.out.println(operation(2,4,3));
     }
-    public static void printDay(String[] a) {
+    public static void printDays(String[] a) {
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
     }
+
     public static int sum(int a, int b) {
         return a + b;
     }
+
     public static int product(int a, int b) {
         return a * b;
     }
-    public static int appleOperation(int a, int b, int operation) {
+
+    public static int operation(int a, int b, int operation) {
         if (operation == 1) {
             return sum(a, b);
         }
@@ -41,6 +42,7 @@ public class Main {
         }
         return stepen(a, b);
     }
+
     public static int stepen(int a, int b) {
         int chislo = a;
         for (int i = 1; i < b; i++) {
