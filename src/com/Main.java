558f580b3d2171cffea69fedf7b334be84d53b25
89081314528 <89081314528@mail.ru>
2019-05-15 21:16:25 +0300

    изменение 15/05

--- src/com/Main.java
+++ src/com/Main.java
@@ -10,20 +10,20 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
-    String[] day = new String[4];
-    day[0] = "a";
-    day[1] = "d";
-    day[2] = "c";
-    day[3] = "v";
-    printDay(day);
+        String[] day = new String[4];
+        day[0] = "a";
+        day[1] = "b";
+        day[2] = "c";
+        day[3] = "d";
+        printDay(day);
         System.out.println(applyOperation(4,2,1));
         System.out.println(applyOperation(4,2,2));
-        System.out.println(applyOperation(2,4, 3));
+        System.out.println(applyOperation(2,5,3 ));
     }
 
-    public static void printDay(String[] g) {
-        for (int i = 0; i < g.length; i++) {
-            System.out.println(g[i]);
+    public static void printDay(String[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
         }
     }
 
@@ -39,7 +39,7 @@ public class Main {
         if (operation == 1) {
             return sum(a, b);
         }
-        if (operation ==2) {
+        if (operation == 2) {
             return product(a, b);
         }
         return stepen(a, b);
@@ -52,5 +52,4 @@ public class Main {
         }
         return a;
     }
-
 }
